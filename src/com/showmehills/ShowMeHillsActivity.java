b7040fd2e66161371bc0cff18c593d723bef0edb
Nicholas Cain <nik@nikcain.net>
2012-04-21 11:44:24 +0100

    Add licensing comment

--- src/com/showmehills/ShowMeHillsActivity.java
+++ src/com/showmehills/ShowMeHillsActivity.java
@@ -1,3 +1,22 @@
+/*
+    Copyright 2012 Nik Cain nik@showmehills.com
+    
+    This file is part of ShowMeHills.
+
+    ShowMeHills is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ShowMeHills is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ShowMeHills.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.showmehills;
 
 /*
